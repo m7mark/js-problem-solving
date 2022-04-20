b1c111824898dc9172c68d6812362493bcfb7d82
m7mark <m7mark@yandex.ru>
2022-04-20 22:24:32 +0300

    Prime check

---
+++ PrimeCheck.js
@@ -0,0 +1,12 @@
+
+const primeArray = (num) => [...Array(num + 1).keys()].filter(isPrime)
+
+const isPrime = (n) => {
+  for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+    if (n % i === 0) return false
+  }
+  return n > 1
+}
+
+console.log(primeArray(10));
+console.log(primeArray(97));
