9451e690f48458d7b24d68ce25f06f5adae817a9
m7mark <m7mark@yandex.ru>
2022-05-13 00:41:11 +0300

    shuffle

--- LearnJavaScript.js
+++ LearnJavaScript.js
@@ -70,6 +70,7 @@ console.log(+rabbit, rabbit + 4, rabbit + '33');
 // ---------------- DATA TYPES
 // https://learn.javascript.ru/data-types
 
+//numbers
 console.log(10..toString(2))
 console.log(0.1 + 0.2);
 console.log(isFinite('4e4'));
@@ -97,3 +98,52 @@ console.log(arr.filter(e => e > 1));
 
 console.log(arr.map(e => e + '$'));
 console.log(arr.reduce((acc, el) => acc + el));
+//create object with map
+console.log(arr.map(el => ({ number: el })));
+//Перебираемые объекты и псевдомассивы
+for (el of arr) {
+  console.log(el);
+}
+
+const rangeIterable = {
+  from: 2,
+  to: 8,
+  [Symbol.iterator]() {
+    this.current = this.from;
+    return this
+  },
+  next() {
+    if (this.current <= this.to) {
+      return { done: false, value: this.current++ }
+    } else {
+      return { done: true }
+    }
+  }
+}
+for (el of rangeIterable) {
+  console.log(el);
+}
+const arrLike = {
+  0: 'First',
+  1: 'Second',
+  length: 2
+}
+console.log(Array.from(rangeIterable));
+console.log(Array.from(arrLike));
+console.log(Object.getOwnPropertySymbols(rangeIterable))
+
+//Map и Set
+let map = new Map()
+map.set(arr, 2)
+console.log(map);
+map = new Map(Object.entries(rangeIterable))
+console.log([...map.values()])
+console.log([...map.keys()])
+console.log([...map.entries()])
+map.delete('next')
+const newObjFromMap = Object.fromEntries(map)
+console.log(newObjFromMap.from);
+map.forEach((val, key) => { console.log(key, val) });
+
+let set = new Set(arr)
+console.log(set, [...set.entries()]);
