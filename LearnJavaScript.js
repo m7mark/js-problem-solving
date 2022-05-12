eb7bed2b344bddd9f0ada5b211ade92a1d3d270d
m7mark <m7mark@yandex.ru>
2022-05-12 19:04:19 +0300

    arrays

--- LearnJavaScript.js
+++ LearnJavaScript.js
@@ -66,3 +66,34 @@ const rabbit = {
 console.log(rabbit[id], Object.keys(rabbit))
 console.log(Reflect.ownKeys(rabbit));
 console.log(+rabbit, rabbit + 4, rabbit + '33');
+
+// ---------------- DATA TYPES
+// https://learn.javascript.ru/data-types
+
+console.log(10..toString(2))
+console.log(0.1 + 0.2);
+console.log(isFinite('4e4'));
+console.log(parseInt('100px'), parseFloat('1.1.1.1'));
+
+//strings
+const str = 'New string data from state'
+console.log(str.indexOf('st'), str.includes('st'));
+console.log(str.slice(0, 3), str.slice(-5));
+console.log('Österreich'.localeCompare('Zenden'))
+
+//Arrays
+console.log([...new Array(5).keys()]);
+const arr = [1, 2]
+arr.push(function () { console.log(this) })
+arr[2]();
+
+console.log(arr.slice(1, 3))
+arr.splice(2, 1, 3) //mutate array
+console.log(arr);
+console.log(arr.concat([4, 5, 6]))
+console.log(arr.includes(3));
+console.log(arr.find(e => e > 1));
+console.log(arr.filter(e => e > 1));
+
+console.log(arr.map(e => e + '$'));
+console.log(arr.reduce((acc, el) => acc + el));
