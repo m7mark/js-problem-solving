4cb095ccb03dd030a5324f1f237a8b4f1c3304d0
m7mark <m7mark@yandex.ru>
2022-05-18 01:45:25 +0300

    NEF

--- LearnJavaScriptFunctions.js
+++ LearnJavaScriptFunctions.js
@@ -2,11 +2,12 @@
 function pow(x, n) {
   return (n === 1) ? x : x * pow(x, n - 1)
 }
-console.log(pow(2, 997))
+console.log(pow(2, 5))
+
 function factorial(n) {
   return (n === 1) ? 1 : n * factorial(n - 1)
 }
-console.log(factorial(10));
+console.log(factorial(5));
 
 // Остаточные параметры и оператор расширения
 function sumAll(...args) {
@@ -17,3 +18,59 @@ function sumAll(...args) {
 }
 console.log(sumAll(1, 4, 6, 2, 1));
 
+//[[Environment]]
+const arr = [1, 3, 6, 8, 10, 22, 44]
+function inBetween(a, b) {
+  return function (x) {
+    return x > a && x < b
+  }
+}
+console.log(arr.filter(inBetween(2, 11)));
+
+//Global
+var globVar = 'Global'
+globalThis.glob = 'Hello global'
+console.log(globalThis.globVar, globalThis.glob)
+
+//Объект функции, NFE
+function makeCounter() {
+  function count() { count.count++ }
+  count.count = 0
+  return count
+}
+let counter = makeCounter()
+counter();
+counter()
+console.log(counter.count);
+console.log(counter.name, counter.length);
+console.log(makeCounter.name);
+console.log(typeof (counter.length));
+
+let greetings = function func(name) {
+  if (name) {
+    return `Hi ${name}`
+  }
+  else {
+    return func('Guest')
+  }
+}
+console.log(greetings())
+console.log(greetings('Gimmy'))
+
+function sum(a) {
+  let currentSum = a
+  function f(b) {
+    if (!b || typeof (b) !== 'number') currentSum += 0
+    else currentSum += b;
+    return f;
+  }
+  f.toString = function () {
+    return currentSum;
+  };
+  return f;
+}
+console.log(sum(9)(1)(1)()()(4)('dd').toString())
+
+//new Function
+const divide = new Function('a,b', 'return a/b')
+console.log(divide(4, 8));
