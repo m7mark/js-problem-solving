00fe73c28bdd15fc9bb1a68d9b11e4fccf40414f
m7mark <m7mark@yandex.ru>
2022-05-31 01:05:51 +0300

    proxy reflect

--- LearnJavaScriptDiff.js
+++ LearnJavaScriptDiff.js
@@ -1,3 +1,6 @@
+// ---------------- GENERATORS, PROXY, REFLECT
+// https://learn.javascript.ru/generators-iterators
+
 function* generator() {
   yield 1;
   yield 2;
@@ -31,7 +34,7 @@ console.log(genn.next());
 //Async generator
 async function* asyncgenerator(start, end) {
   for (let i = start; i <= end; i++) {
-    await new Promise(resolve => setTimeout(() => resolve(), 500))
+    await new Promise(resolve => setTimeout(() => resolve(), 200))
     yield i;
   }
 }
@@ -44,7 +47,7 @@ const asyncrange = {
   to: 3,
   async *[Symbol.asyncIterator]() {
     for (let val = this.from; val <= this.to; val++) {
-      await new Promise(resolve => setTimeout(resolve, 500))
+      await new Promise(resolve => setTimeout(resolve, 200))
       yield val;
     }
   }
@@ -52,3 +55,55 @@ const asyncrange = {
 for await (let val of asyncrange) {
   console.log(val);
 }
+
+//Proxy
+let numbers = [1, 2, 3]
+numbers = new Proxy(numbers, {
+  get(target, p) {
+    if (p in target) { return target[p] }
+    else { return 0 }
+  }
+})
+console.log(numbers[2], numbers[4]);
+
+function delay(f, ms) {
+  return new Proxy(f, {
+    apply(target, thisArgs, args) {
+      setTimeout(() => target.apply(thisArgs, args), ms)
+    }
+  })
+}
+let say = () => {
+  console.log('Hello delay');
+}
+say = delay(say, 400)
+say()
+
+//Reflect
+class RefUser {
+  #name = 'Jakob';
+  getName() {
+    return this.#name
+  }
+}
+let refUser = new RefUser()
+console.log(refUser.getName());
+refUser = new Proxy(refUser, {
+  get(target, prop, reciever) {
+    let value = Reflect.get(...arguments)
+    return (typeof value == 'function') ? value.bind(target) : value
+  }
+})
+console.log(refUser.getName());
+
+//Отключаемые прокси
+let obj = {
+  data: 'Important!'
+}
+const revokes = new WeakMap()
+let { proxy, revoke } = Proxy.revocable(obj, {})
+revokes.set(proxy, revoke)
+console.log(proxy.data);
+revoke = revokes.get(proxy)
+revoke()
+console.log(proxy.data);
